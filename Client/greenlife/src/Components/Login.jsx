036cefa1b7e6d559975f1710bd12103a7a648737
KarthikaMovva <karthika.movva@kalvium.community>
2024-05-02 21:35:54 +0530

    added css to login and signup pages

--- Client/greenlife/src/Components/Login.jsx
+++ Client/greenlife/src/Components/Login.jsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom";
+import Navbar from "./Navbar";
 function Login(){
     return(
-        <div>
+        <div className="flex">          
+            <div className="bg-left w-full h-screen">
+                <img src="https://imgcdn.floweraura.com/types-of-indoor-plant-types-cover-image.jpg" alt="" className="w-full h-full"/>
+            </div>
+        <div className="absolute left-1/3 top-1/3 border-2 black bg-white pr-20 pt-20 pl-20 pb-20">
+            <div>
+            <Navbar/>
+            </div>
             <div>
             <h3>Login</h3>
             <input type="text" className="border-2 black mb-5" placeholder="UserName"/>
@@ -11,6 +20,12 @@ function Login(){
             <div>
             <input type="text" className="border-2 black mb-5" placeholder="Password"/>
             </div>
+            <NavLink to="/order">
+            <button className="bg-yellow-300">Login</button>
+            </NavLink>
+            <p>If, you did not have a account prefer to <a href="/signup">Signup</a></p>
+        </div>
+        <div className="bg-right bg-green-400 h-screen w-full"></div>
         </div>
     )
 }
--- Client/greenlife/src/Components/Navbar.jsx
+++ Client/greenlife/src/Components/Navbar.jsx
@@ -1,27 +1,25 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-function Main(){
+function Navbar(){
     return(
-        <div className='mainbody'>
+  <div className='mainbody'>
         <nav className='flex items-center bg-red-50 h-12 w-full fixed top-0 left-0 justify-around'>
+        <NavLink to="/">
           <p><img src="https://i.postimg.cc/4N1Xqpyx/Capstone-Project-1-removebg-preview.png" alt="" className='h-12 w-35 cursor-pointer'/></p>
-          <Link to="/login">
+          </NavLink>
+          <NavLink to="/login">
           <p className=' text-xl cursor-pointer'>Login</p>
-          </Link>
-          <Link to="/Signup">
+          </NavLink>
+          <NavLink to="/Signup">
           <p className='cursor-pointer text-xl'>SignUp</p>
-          </Link>
-          <Link to="/Blog">
+          </NavLink>
+          <NavLink to="/Blog">
           <p className='cursor-pointer text-xl'>Blog</p>
-          </Link>
+          </NavLink>
         </nav>
         <div className='bg-green-900 text-white fixed w-full left-0 text-center h-30 top-12'>Create a spring in your life.</div>
-        <div><img src="https://i.postimg.cc/K8DBCzvG/Add1.png" alt="" className='mt-30px h-400px w-full'/></div>
-        <Link to="/order">
-        <button className='border-yellow-400 bg-yellow-400 m-10'>Start Planting now!!</button>
-        </Link>
         </div>
     )
 }
 
-export default Main
+export default Navbar;
