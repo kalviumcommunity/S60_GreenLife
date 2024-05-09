a9208e0fe3e6a9b3fe83e7a20c969af3abd857e1
KarthikaMovva <karthika.movva@kalvium.community>
2024-05-09 23:14:00 +0530

    added 10 entities to database

--- Server/mongoConnect.js
+++ Server/mongoConnect.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
+const PlantsData = require("./PlantsData")
 dotenv.config()
 
 function ConnectDatabase(){
@@ -15,8 +16,22 @@ mongoose.connect(process.env.Connection_String)
 
 const plantSchema=mongoose.Schema({
     PlantName:String,
+    ScientificName:String,
     PlantType:String,
-    PlantCost:String
+    FamilyName:String,
+    PlantCost:String,
+    ReferenceLink:String,
+    Uses : String,
+    Toxicity : String,
+    Cautions : String,
+    WateringTips : String,
+    NeedOfSunlight : String,
+    PlantImage : String
 })
 const PlantModel=mongoose.model("plants",plantSchema)
+
+PlantModel.insertMany(PlantsData)
+.then(()=>console.log("plants data is sended to database"))
+.catch((err)=>console.log("database error:",err))
+
 module.exports={Connection: ConnectDatabase, SchemaModel : PlantModel};
