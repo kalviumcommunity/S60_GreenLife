7492f16420e2f4c07faf5f0338e16d6fbf8b2cee
KarthikaMovva <karthika.movva@kalvium.community>
2024-05-11 17:09:26 +0530

    connected get request to database in backend

--- Server/routes.js
+++ Server/routes.js
@@ -1,7 +1,11 @@
 const express=require("express");
 const app=express();
+const {SchemaModel}=require("./mongoConnect")
 app.get("/get",(request,response)=>{
-    response.send("This is a get request")
+    // response.send("This is a get request")
+    SchemaModel.find({})
+    .then((plantlist)=>{response.json({plantlist})})
+    .catch((err)=>{response.json({err})})
 })
 app.put("/put/:id",(request,response)=>{
     response.send("This is a put request")
