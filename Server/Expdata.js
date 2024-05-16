fe853354b9e096cdb031635189456603892918b9
KarthikaMovva <karthika.movva@kalvium.community>
2024-05-16 22:16:48 +0530

    added new collection to make post request

---
+++ Server/Expdata.js
@@ -0,0 +1,8 @@
+const Experience=[
+    {
+        experience : "Welcome to our plant-loving community!🌿 Whether you're a seasoned gardener or just starting your green journey, we invite you to share your plant experiences here. 🌱🌼At our online plant store, we believe that every leaf tells a story. From nurturing tiny succulents on your windowsill to watching majestic monstera leaves unfurl, your journey with plants is unique and inspiring. Share your tips, triumphs, and even the occasional leafy mishap-because every browned edge is a lesson learned! 🌿🌱Connect with fellow plant enthusiasts, swap propagation secrets, and discover new varieties. Did your fiddle leaf fig finally sprout a new leaf? Share the joy! Or perhaps you've mastered the art of keeping orchids happy - enlighten us! 🌸Together, let's cultivate a space where plant love blooms. So grab your watering can, dust off your trowel, and let's grow together! 🌿🌱🌸",
+        image : "https://bloximages.chicago2.vip.townnews.com/newsmirror.net/content/tncms/assets/v3/editorial/7/39/739dd1f4-84c1-11ea-8ad9-2b31c89d2ac7/5ea08395314e6.image.jpg?resize=1200%2C801"
+    }
+]
+
+module.exports=Experience;
--- Server/routes.js
+++ Server/routes.js
@@ -1,19 +1,30 @@
 const express=require("express");
 const app=express();
-const {SchemaModel}=require("./mongoConnect")
+const {SchemaModel,ExpModel}=require("./mongoConnect")
+
 app.get("/get",(request,response)=>{
     // response.send("This is a get request")
     SchemaModel.find({})
     .then((plantlist)=>{response.json({plantlist})})
     .catch((err)=>{response.json({err})})
 })
+
+app.get("/getexp",(request,response)=>{
+    ExpModel.find({})
+    .then((listexp)=>{response.json({listexp})})
+    .catch((err)=>{response.json({err})})
+})
+
 app.put("/put/:id",(request,response)=>{
     response.send("This is a put request")
 })
+
 app.post("/post",(request,response)=>{
     response.send("This  is a post request")
 })
+
 app.delete("/delete",(request,response)=>{
     response.send("This is a delete request")
 })
+
 module.exports=app;
