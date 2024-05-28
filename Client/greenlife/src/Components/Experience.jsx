906187993e9653cfb748d3b54cbc9f8555ccec9b
KarthikaMovva <karthika.movva@kalvium.community>
2024-05-28 15:05:04 +0530

    created a put request

--- Client/greenlife/src/Components/Experience.jsx
+++ Client/greenlife/src/Components/Experience.jsx
@@ -6,6 +6,11 @@ function Experience(){
 const[users,setusers]=useState([])
 const[experience,setexperience]=useState("")
 const[file,setfile]=useState(null)
+const[needtoupdate,setneedtoupdate]=useState(false);
+const[updateExp,setupdateExp]=useState("");
+const[updatefile,setupdatefile]=useState(null);
+const[id,setid]=useState(null)
+
 
 useEffect(()=>{
     const getusers=async()=>{
@@ -19,6 +24,34 @@ useEffect(()=>{
     }
     getusers()
 },[])
+
+
+const UpdateRequest=async()=>{
+    if (!id) return; 
+    const userneedupdate=users.find(user=>user._id===id)
+    if(!userneedupdate) return;
+    try{
+        let image=updatefile;
+        if(updatefile && updatefile!==users.find(user=>user._id===id).image){
+        var createdData=new FormData();
+        createdData.append("file",updatefile);
+        createdData.append("upload_preset","x31quij6");
+        var generatefilelink=await axios.post("https://api.cloudinary.com/v1_1/dg6izvre4/image/upload",createdData);
+        image=generatefilelink.data.secure_url;
+    }
+    var updatedData=await axios.put(`http://localhost:3000/updateExp/${id}`,{experience : updateExp,image})
+    const updatedExp=updatedData.data
+    setusers(users.map((user) => user._id === id ? { ...user, experience: updatedExp.experience, image: updatedExp.image } : user));
+    console.log(users)
+    setneedtoupdate(false);
+    setupdateExp("");
+    setupdatefile(null);
+    setid(null);
+}
+    catch(err){
+console.log("Update request err:",err) 
+    }
+}
 
 function handleinput(event){
 setexperience(event.target.value)
@@ -62,6 +95,9 @@ try{
     console.log("delete err:",err)
 }
 }
+
+console.log(users)
+
     return(
         <div>
             <div>
@@ -69,12 +105,18 @@ try{
             </div>
             <div>
         {users.map((user)=>{
+            if (!user) return null;
             return(
                 <div key={user._id} className="rounded-3xl border-2 mt-20 p-4 flex h-72 bg-slate-100">
                   <p className="m-5">{user.experience}</p>
                  <img src={user.image} alt="" className="h-50 w-70 rounded-3xl"/>
                  <div>
-                 <button className="mb-20">Update</button>
+                 <button className="mb-20" onClick={()=>{
+                    setneedtoupdate(true);
+                    setupdateExp(user.experience);
+                    setupdatefile(user.image);
+                    setid(user._id);
+                 } }>Update</button>
                  <button onClick={()=>deleteRequest(user._id)} className="mt-20">Delete</button>
                  </div>
                 </div>
@@ -84,10 +126,10 @@ try{
         <div className="flex justify-center fixed bottom-0 left-0 w-11/12 ">
         <label htmlFor="inputUrfiles" className="cursor-pointer">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRxjZhawty_IUsjt2xke_qk2AIZCpVd7luGJrTo-emag&s" alt="" className="w-12 h-12 cursor-pointer"/>   
-        <input type="file" onChange={handlefileUpload} id="inputUrfiles" accept="image/*" className="hidden"/>
+        <input type="file" onChange={needtoupdate?(e)=>setupdatefile(e.target.files[0]):handlefileUpload} id="inputUrfiles" accept="image/*" className="hidden"/>
         </label>
-        <input type="text" className="border-black p-3 mb-4 w-3/4 bg-green-200"  onChange={handleinput} placeholder="Enter you experience" value={experience}/>
-        <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-squares-01/3/89-512.png" alt="" className="w-12 h-12 cursor-pointer" onClick={postUrExp}/>
+        <input type="text" className="border-black p-3 mb-4 w-3/4 bg-green-200"  onChange={needtoupdate?(e)=>setupdateExp(e.target.value):handleinput} placeholder="Enter you experience"   value={needtoupdate ? updateExp : experience}/>
+        <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-squares-01/3/89-512.png" alt="" className="w-12 h-12 cursor-pointer" onClick={needtoupdate?UpdateRequest:postUrExp}/>
         </div>
         </div>
     )
